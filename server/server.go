2ad3306bc6abb6199965e2a8653e73808d205f38
Dominik Ágh <agh.dominik@gmail.com>
2022-12-28 14:43:30 +0100

    feat: Refactor templates & add about page

--- server/server.go
+++ server/server.go
@@ -15,8 +15,14 @@ type ServerConfig struct {
 	Port int
 }
 
-type StatusData struct {
-	Feed []template.HTML
+type Post struct {
+	Time    int
+	Content []byte
+}
+
+type FeedData struct {
+	Title string
+	Feed  []template.HTML
 }
 
 func initConfig() ServerConfig {
@@ -32,19 +38,37 @@ func Run() {
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/layout.html"))
+		tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/page.html"))
 
-		md := []byte("## Testing \nStatus **update**")
-		md2 := []byte("Another _status_: [link](https://example.com)")
-		sd := StatusData{
-			Feed: []template.HTML{
-				template.HTML(markdown.ToHTML(md, nil, nil)),
-				template.HTML(markdown.ToHTML(md2, nil, nil)),
+		posts := []Post{
+			{
+				Time:    123456789,
+				Content: []byte(`First **post**`),
+			},
+			{
+				Time:    234567890,
+				Content: []byte(`Second *post*`),
+			},
+			{
+				Time:    345678901,
+				Content: []byte("Third `post`"),
 			},
 		}
-		tmpl.Execute(w, sd)
+		fd := FeedData{
+			Title: "Dom's current",
+		}
+		for _, p := range posts {
+			fd.Feed = append(fd.Feed, template.HTML(markdown.ToHTML(p.Content, nil, nil)))
+		}
+		tmpl.ExecuteTemplate(w, "page", fd)
 	})
 
+	r.Get("/about", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/about.html", "templates/page.html"))
+		tmpl.ExecuteTemplate(w, "page", nil)
+	})
+
+	// server static files
 	fs := http.FileServer(http.Dir("static/"))
 	r.Handle("/s/*", http.StripPrefix("/s/", fs))
 
