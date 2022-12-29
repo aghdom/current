22034a8fe31ba1c0da6250d72ea53f10f123b87e
Dominik Ágh <agh.dominik@gmail.com>
2022-12-29 15:23:06 +0100

    wip: Filtering on date & timestamp

--- server/server.go
+++ server/server.go
@@ -7,6 +7,7 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -63,7 +64,7 @@ func initConfig() ServerConfig {
 
 func transformPost(post Post) FeedPost {
 	return FeedPost{
-		Date:    post.Time.Format("2006/02/01"),
+		Date:    post.Time.Format("2006/01/02"),
 		Time:    post.Time.Format("15:04"),
 		Unix:    post.Time.Unix(),
 		Content: template.HTML(markdown.ToHTML(post.Content, nil, nil)),
@@ -72,6 +73,16 @@ func transformPost(post Post) FeedPost {
 
 //go:embed static/*
 var staticFS embed.FS
+
+func inTimeSpan(start, end, check time.Time) bool {
+	if start.Before(end) {
+		return !check.Before(start) && !check.After(end)
+	}
+	if start.Equal(end) {
+		return check.Equal(start)
+	}
+	return !start.After(check) || !end.Before(check)
+}
 
 func Run() {
 	cfg := initConfig()
@@ -92,6 +103,69 @@ func Run() {
 	r.Get("/about", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/about.html", "templates/page.html"))
 		tmpl.ExecuteTemplate(w, "page", nil)
+	})
+
+	r.Get("/posts/{timestamp}", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/page.html"))
+		var tm time.Time
+		ts := chi.URLParam(r, "timestamp")
+		if ts != "" {
+			unix, err := strconv.ParseInt(ts, 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tm = time.Unix(unix, 0).UTC()
+		}
+
+		pd := PageData{
+			Title: "Dom's current",
+		}
+
+		for _, p := range getPosts() {
+			if p.Time == tm {
+				pd.Feed = append(pd.Feed, transformPost(p))
+				pd.Title = p.Time.Format("2006/02/01 15:04")
+			}
+		}
+		tmpl.ExecuteTemplate(w, "page", pd)
+	})
+
+	r.Get("/on/{year}/{month}/{day}", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/page.html"))
+		var year, month, day int64
+		var err error
+		y, m, d := chi.URLParam(r, "year"), chi.URLParam(r, "month"), chi.URLParam(r, "day")
+
+		if y != "" {
+			year, err = strconv.ParseInt(y, 10, 0)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		if m != "" {
+			month, err = strconv.ParseInt(m, 10, 0)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		if d != "" {
+			day, err = strconv.ParseInt(d, 10, 0)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		tm := time.Date(int(year), time.Month(month), int(day), 0, 0, 0, 0, time.UTC)
+		pd := PageData{
+			Title: "Posts on " + tm.Format("2006/01/02"),
+		}
+
+		for _, p := range getPosts() {
+			if inTimeSpan(tm, tm.Add(24*time.Hour), p.Time) {
+				pd.Feed = append(pd.Feed, transformPost(p))
+			}
+		}
+		tmpl.ExecuteTemplate(w, "page", pd)
 	})
 
 	// serve embeded static files
